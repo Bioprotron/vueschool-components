ee2b2855f4678fb458af9a759e81bcbaa083d298
unknown <brkbenjamin@yandex.ru>
2023-12-25 00:26:38 +0300

    vueschool-components

---
+++ app.js
@@ -0,0 +1,38 @@
+let PlanPickerItemComponent =  {
+    template: '#plan-picker-item-template',
+    props: {
+        name: {type: String, required: true},  
+        selected: {type: Boolean, default: false}    
+    },  
+    methods: {
+        select() {           
+            this.$emit('selected', this.name)
+        }
+    }
+}
+
+let PlanPickerComponent = {   
+    components: {
+        'plan-picker-item': PlanPickerItemComponent
+    },  
+    template: '#plan-picker-template',
+    data() {
+        return {
+            plans: ['The Single', 'The Curious', 'The Addict', ],
+            selectedPlan: null
+        }
+    },
+    methods: {
+        selectPlan(plan) {
+            this.selectedPlan = plan
+        }
+    }   
+}
+
+
+const app = Vue.createApp({
+  components: {
+    'plan-picker': PlanPickerComponent,
+  }
+})
+.mount('#app')
